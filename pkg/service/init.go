d6b4865e81f1deda7330b766bc9db5ac7d779bf9
Nelson Oliveira <nelson@imd.ufrn.br>
2025-05-26 15:55:08 -0300

    fix: typo on log message

--- pkg/service/init.go
+++ pkg/service/init.go
@@ -93,7 +93,7 @@ func (a *TngfApp) Start(tlsKeyLogPath string) {
 	logger.InitLog.Infoln("Server started")
 
 	if err := a.InitDefaultXfrmInterface(); err != nil {
-		logger.InitLog.Errorf("Initicating XFRM interface for control plane failed: %+v", err)
+		logger.InitLog.Errorf("Initiating XFRM interface for control plane failed: %+v", err)
 		return
 	}
 
