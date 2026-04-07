97f6ea77c8110b32ea6ff898460ce0c86d6a6e47
Ng Warren <ngwarrencinyen.ee14@nycu.edu.tw>
2026-04-07 16:18:03 +0800

    fix: drop short UDP/4500 NAT-T packets in IKE dispatcher (#942)

---
+++ pkg/ike/dispatcher_test.go
@@ -0,0 +1,21 @@
+package ike_test
+
+import (
+	"net"
+	"testing"
+
+	"github.com/free5gc/tngf/pkg/ike"
+)
+
+func TestDispatchDropsShortUDP4500PacketWithoutPanic(t *testing.T) {
+	localAddr := &net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 4500}
+	remoteAddr := &net.UDPAddr{IP: net.IPv4(127, 0, 0, 2), Port: 12345}
+
+	defer func() {
+		if p := recover(); p != nil {
+			t.Fatalf("Dispatch panicked on short UDP/4500 packet: %v", p)
+		}
+	}()
+
+	ike.Dispatch(nil, localAddr, remoteAddr, []byte{0x00, 0x00, 0x00})
+}
